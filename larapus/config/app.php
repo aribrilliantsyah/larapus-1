886cef186532a7daaa30583dcfddb43471d8f3d3
rhidayat122 <151610243@smkassalaambandung.sch.id>
2017-06-13 15:05:53 +0700

    pembaruan

--- larapus/config/app.php
+++ larapus/config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'My Application',
+    'name' => 'Larapus',
 
     /*
     |--------------------------------------------------------------------------
@@ -165,8 +165,9 @@ return [
 
         /*
          * Package Service Providers...
-         */
 
+         */
+        Collective\Html\HtmlServiceProvider::class,
         //
 
         /*
@@ -224,6 +225,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
--- larapus/resources/views/home.blade.php
+++ larapus/resources/views/home.blade.php
@@ -2,12 +2,13 @@
 
 @section('content')
 <div class="container">
-     <div class="row">
+    <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-              <div class="panel-heading">Halo</div>
-                <div class="panel_body">
-                    Selamat datang di Larapus
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    Anda berhasil login!
                 </div>
             </div>
         </div>
